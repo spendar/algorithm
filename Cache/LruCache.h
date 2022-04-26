c423fdf7625377a25c0d17fabd07885cbf6d3562
：Spendar： <alexander1214idol@gmail.com>
2022-04-26 21:29:13 +0800

    修正了后台清理线程存在的一些问题

--- Cache/LruCache.h
+++ Cache/LruCache.h
@@ -1,7 +1,7 @@
 /*
  * @Author: spendar
  * @Date: 2022-04-04 21:21:35
- * @LastEditTime: 2022-04-05 12:49:00
+ * @LastEditTime: 2022-04-26 18:15:47
  * @LastEditors: Please set LastEditors
  * @Description: to implement a lru cache with expired time
  * @FilePath: \Cache\LruCache.h
@@ -31,28 +31,29 @@ template<typename K, typename V>
 class LruCache {
 public:
     LruCache(int capacity, int ttl = 10000) : capacity_(capacity), ttl_(ttl), size_(0), isStop_(false) {
-        head = new Node<K,V>();
-        tail = new Node<K,V>();
-        head->next = tail;
-        tail->pre = head;
+        head_ = new Node<K,V>();
+        tail_ = new Node<K,V>();
+        head_->next = tail_;
+        tail_->pre = head_;
     }
     
     ~LruCache() {
         isStop_ = true;
-        daemon->join();
+        daemon_->join();
     }
 
     bool get(K key, V& data) {
-        std::lock_guard<std::mutex> locker(mtx);
-        auto iter = m.find(key);
-        if(iter == m.end()) {
+        std::lock_guard<std::mutex> locker(mtx_);
+        auto iter = m_.find(key);
+        if(iter == m_.end()) {
             return false;
         } else {
             Node<K, V>* temp = iter->second;
             if(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - temp->timestamp).count() > ttl_) {
-                m.erase(key);
+                m_.erase(key);
                 erase(temp);
                 delete temp;
+                size_--;
                 return false;
             }
             erase(temp);
@@ -63,21 +64,22 @@ public:
     }
 
     void put(K key, V val) {
-        std::lock_guard<std::mutex> locker(mtx);
-        auto iter = m.find(key);
-        if(iter == m.end()) {
+        std::lock_guard<std::mutex> locker(mtx_);
+        auto iter = m_.find(key);
+        if(iter == m_.end()) {
             if(size_ >= capacity_) {
-                Node<K,V>* temp = tail->pre;
+                Node<K,V>* temp = tail_->pre;
                 erase(temp);
                 delete temp;
+                m_.erase(temp->key);
                 size_--;
             }
             Node<K, V>* temp = new Node<K,V>(key, val, std::chrono::steady_clock::now());
             add(temp);
-            m[key] = temp;
+            m_[key] = temp;
             size_++;
         } else {
-            Node<K, V>* temp = m[key];
+            Node<K, V>* temp = m_[key];
             erase(temp);
             temp->val = val;
             temp->timestamp = std::chrono::steady_clock::now();
@@ -86,13 +88,13 @@ public:
     }
 
     int size() {
-        std::lock_guard<std::mutex> locker(mtx);
+        std::lock_guard<std::mutex> locker(mtx_);
         return size_;
     }
 
     void print() {
-        Node<K, V>* cur = head->next;
-        while(cur != tail) {
+        Node<K, V>* cur = head_->next;
+        while(cur != tail_) {
             std::cout<<cur->key<<" ";
             cur = cur->next;
         }
@@ -103,16 +105,17 @@ public:
         while(!isStop_) {
             std::this_thread::sleep_for(std::chrono:: milliseconds (1000));
             {
-                std::lock_guard<std::mutex> locker(mtx);
+                std::lock_guard<std::mutex> locker(mtx_);
                 // 清理过期键值对
-                Node<K, V>* cur = head->next;
-                while(cur != tail) {
+                Node<K, V>* cur = head_->next;
+                while(cur != tail_) {
                     // std::cout<<cur->key<<" ";
                     Node<K, V>* temp = cur->next;
                     if(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - cur->timestamp).count() > ttl_) {
                         erase(cur);
-                        m.erase(cur->key);
+                        m_.erase(cur->key);
                         delete cur;
+                        size_--;
                     }
                     cur = temp;
                 }
@@ -128,10 +131,10 @@ private:
     }
 
     void add(Node<K, V>* node) {
-        head->next->pre = node;
-        node->next = head->next;
-        node->pre = head;
-        head->next = node;
+        head_->next->pre = node;
+        node->next = head_->next;
+        node->pre = head_;
+        head_->next = node;
     }
 
 private:
@@ -139,9 +142,9 @@ private:
     int size_;
     bool isStop_;    
     int ttl_;
-    std::mutex mtx;
-    std::unordered_map<K, Node<K, V>*> m;
-    Node<K, V>* head;
-    Node<K, V>* tail;
-    std::unique_ptr<std::thread> daemon = std::make_unique<std::thread>(&LruCache<K, V>::clean, this);
+    std::mutex mtx_;
+    std::unordered_map<K, Node<K, V>*> m_;
+    Node<K, V>* head_;
+    Node<K, V>* tail_;
+    std::unique_ptr<std::thread> daemon_ = std::make_unique<std::thread>(&LruCache<K, V>::clean, this);
 };
